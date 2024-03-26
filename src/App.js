a3d2dc25e41526a9c97e4c83fddb3ca0ba111d31
Endar Adi Sasmito <endar@Endars-MacBook-Air.local>
2024-03-27 01:09:33 +0700

    tes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          yess manasdasdkjbk
+          yess manasdasdkjbkasdasdasdaasdasdasdaßß
         </a>
       </header>
     </div>
